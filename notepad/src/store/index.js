54481e79f5d7a67496cd8baf2e005e3f9a958f24
Atsushi-Endo <ena-choco@live.jp>
2020-09-02 14:59:08 +0900

    メモを実際に保存できるようにする

--- notepad/src/store/index.js
+++ notepad/src/store/index.js
@@ -8,5 +8,15 @@ export default new Vuex.Store({
     memos: [
       {id: 1, body: 'サンプルのメモです'}
     ]
+  },
+  mutations: {
+    // memoをパラメータとして受け取る
+    save(state, memo) {
+      // 新しいIDを割り振る
+      var max = state.memos[state.memos.length -1].id;
+      memo.id = max +1;
+      // stateの中のmemosにmemoをpush(追加)するという意味
+      state.memos.push(memo);
+    }
   }
 })
