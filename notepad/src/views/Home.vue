674328d762e2fbd71c640e601ce799c0b849c215
Atsushi-Endo <ena-choco@live.jp>
2020-09-02 16:07:17 +0900

    scopedの削除とstyleをHomeへ移動

--- notepad/src/views/Home.vue
+++ notepad/src/views/Home.vue
@@ -6,6 +6,40 @@
   </div>
 </template>
 
+<style>
+  textarea{
+    width: 100%;
+    height: 10em;
+  }
+
+  button{
+    border: 1px solid #333;
+    background-color: #333;
+    color: #fff;
+    padding: 10px 20px;
+    margin-top: 10px;
+  }
+</style>
+
+<script>
+export default {
+  name: "new",
+  data: function(){
+    return {
+      memobody: ""
+    }
+  },
+  methods: {
+    save: function(){
+      this.$store.commit("save",{
+        body: this.memobody
+      })
+      this.$router.push("/");
+    }
+  }
+}
+</script>
+
 <script>
 export default {
   name: "home",
